c60358a1d0fa55e79d24e5f9aca63954ada89faf
isnajaya2407 <kristinajaya26@gmail.com>
2025-07-05 16:17:08 +0700

    up

---
+++ Pertemuan10/src/uasisna/dbkoneksi.java
@@ -0,0 +1,33 @@
+
+ 
+package uasisna;
+
+import java.sql.DriverManager;
+import java.sql.Connection  ;    
+import java.sql.SQLException;
+
+/**
+ *
+ * @author isnajaya
+ * TGL 24 Mei 2025
+ */
+public class dbkoneksi {
+    static String DB_DRIVER="com.mysql.cj.jdbc.Driver";
+    static String DB_HOST="jdbc:mysql://localhost:3306/pariwisata";
+    static String DB_USER="root";
+    static String DB_PASSWORD="";
+    
+    static public Connection koneksi(){
+        try{
+            Class.forName(DB_DRIVER);
+            return DriverManager.getConnection(DB_HOST, DB_USER, DB_PASSWORD);
+        }catch(ClassNotFoundException | SQLException err){
+            System.out.println("Gagal koneksi ke DBMS MySQL");
+        }
+        return null;
+    }
+    
+}
+
+    
+
